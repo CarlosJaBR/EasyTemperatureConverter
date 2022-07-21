56c0366e2a98b0364c1f9c4ab3ac5a11388bf775
CarlosJaBr <carjabo12@gmail.com>
2022-07-21 13:38:05 -0500

    exception fixed

--- src/controller/ConverterController.java
+++ src/controller/ConverterController.java
@@ -7,6 +7,8 @@ import application.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.TemperatureConverter;
@@ -32,10 +34,19 @@ public class ConverterController implements Initializable{
     
 	@FXML
     void converterToFahrenheit(ActionEvent event) {
-		String celsius = celsiusGrade.getText();
-		double celsiusConvert = Double.parseDouble(celsius);
-		double fahGrade = tConverter.celsiusToFahrenheit(celsiusConvert);
-		fahrenheitGrade.setText(fahGrade+"°F");
+		
+		try {
+			String celsius = celsiusGrade.getText();
+			double celsiusConvert = Double.parseDouble(celsius);
+			double fahGrade = tConverter.celsiusToFahrenheit(celsiusConvert);
+			fahrenheitGrade.setText(fahGrade+"°F");
+		}catch(NumberFormatException e) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error Dialog");
+			alert.setHeaderText("Look, an Error Dialog");
+			alert.setContentText("Ooops, there was an error!");
+			alert.showAndWait();
+		}
 	
 	}
     
